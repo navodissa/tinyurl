f77b8e253262962768db2d7942f2ed7a0efc5294
Navoda <navomails@gmail.com>
2021-04-25 00:07:14 +0000

    Added base62 encode

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,9 @@ def main():
     b = writeAPI.WriteAPI()
     #b.createDB()
     #b.writeData()
-    b.readData()
+    result = b.readData()
+    new_Url = b.base_encode(int(result, 16))[:7]
+    print (new_Url)
     b.closeConn()
 
 if __name__ == '__main__':
--- backend/writeAPI.py
+++ backend/writeAPI.py
@@ -1,22 +1,36 @@
-# sqlite3 module is already ported with Python3
+# sqlite3, haslib modules are already ported with Python3
 import sqlite3
+import hashlib, math
+import base62
 
 class WriteAPI():
 
     def __init__(self) :
 
         # Make a connection to the DB. Using the current path.
-        # Try to make a relative path instead of absolute path.
-        self.conn = sqlite3.connect('/home/cloud_user/tinyurl/backend/urlmap.db')
+        self.conn = sqlite3.connect('tinyurl/database/urlmap.db')
 
         self.c = self.conn.cursor()
+
+    def calculate(digits):
+        digitsCount = len(digits)
+        hashString = ""
+        i = 0
+        while digitsCount > i:
+            hashString += BASE62ALPHABET[digits[i]] 
+            i = i + 1
+        return hashString
+        
 
     def createDB(self):
         # Create table
         self.c.execute('''CREATE TABLE stocks
                     (date text, trans text, symbol text, qty real, price real)''')
 
-    def writeData(self):
+    def writeData(self, url):
+
+        result = hashlib.sha256(url).hexdigest()
+        print (result)
 
         # Insert a row of data
         self.c.execute("INSERT INTO stocks VALUES ('2007-01-05','BUY','BHAT',100,36.14)")
@@ -25,16 +39,36 @@ class WriteAPI():
         self.conn.commit()
 
     def readData(self):
+
+        url = ('https://www.google.lk').encode()
+        result = hashlib.sha256(url).hexdigest()
+        print (result)
+
         # Query the data
         self.c.execute("SELECT * FROM stocks;")
 
         for date, trans, symbol, qty, price in self.c.fetchall():
             print(date, trans, symbol, qty, price)
 
+        return result    
+
+    def base_encode(self, num, base=62):
+        digits = []
+        while num > 0:
+            remainder = num % base
+            remainder = math.modf(remainder)
+            if remainder[1] > 0:
+                digits.append(int(remainder[1]))
+            num = num / base
+        newUrl = base62.calculate(digits)
+        return newUrl
+
     def closeConn(self):
         # We can also close the connection if we are done with it.
         # Just be sure any changes have been committed or they will be lost.
         self.conn.close()
+    
+    
 
 
 
